e747adb6716755055be8781ac40eb785be7ed8d9
qinghexi <894868351@qq.com>
2016-06-15 18:28:57 +0800

    继承控件

--- src/main/java/com/example/hasee/twoactivity/MainActivity.java
+++ src/main/java/com/example/hasee/twoactivity/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.hasee.twoactivity;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -10,9 +9,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(MainActivity.this, "rilegoule，好吧", Toast.LENGTH_SHORT).show();
-        Toast.makeText(MainActivity.this, "rilegoule，no", Toast.LENGTH_SHORT).show();
-		//alright
 
     }
 }
